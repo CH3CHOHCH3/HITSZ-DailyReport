68ca6ef6fb9d9ac71b48456d595be2fe5b72d806
CH3CHOHCH3 <ybc20020730@gmail.com>
2022-03-13 20:53:15 +0800

    Rich the configuration file

--- DailyReport.sh
+++ DailyReport.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
 # Get user info
-user=$(cat ./user.ini)
-username=${user#*username=}
+info=$(cat ./configure)
+
+username=${info#*username=}
 username=${username%%;*}
-password=${user#*password=}
+
+password=${info#*password=}
 password=${password%%;*}
 
-# Urlencode the password
+status=${info#*status=}
+status=${status%%;*}
+
+longitude=${info#*longitude=}
+longitude=${longitude%%;*}
+
+latitude=${info#*latitude=}
+latitude=${latitude%%;*}
+
+province=${info#*province=}
+province=${province%%;*}
+
+city=${info#*city=}
+city=${city%%;*}
+
+district=${info#*district=}
+district=${district%%;*}
+
+street_number=${info#*street_number=}
+street_number=${street_number%%;*}
+
+vaccine=${info#*vaccine=}
+vaccine=${vaccine%%;*}
+
+
+# Urlencode the info
 password=$(echo -n $password | xxd -p | tr -d '\n' | sed 's/\(..\)/%\1/g')
+province=$(echo -n $province | xxd -p | tr -d '\n' | sed 's/\(..\)/%\1/g')
+city=$(echo -n $city | xxd -p | tr -d '\n' | sed 's/\(..\)/%\1/g')
+street_number=$(echo -n $street_number | xxd -p | tr -d '\n' | sed 's/\(..\)/%\1/g')
 
 # Get jsessionid and LT to log in
 cnt=$(curl -i -s 'https://sso.hitsz.edu.cn:7002/cas/login?service=https%3A%2F%2Fstudent.hitsz.edu.cn%2Fcommon%2FcasLogin%3Fparams%3DL3hnX21vYmlsZS9ob21l')
@@ -139,7 +169,7 @@ res=$(curl -s 'https://student.hitsz.edu.cn/xg_mobile/xsMrsbNew/save' \
   -H 'Referer: https://student.hitsz.edu.cn/xg_mobile/xsMrsbNew/index' \
   -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6' \
   -H "Cookie: JSESSIONID=$jsessionid" \
-  --data-raw "" \
+  --data-raw "info=%7B%22model%22%3A%7B%22dqzt%22%3A%22$status%22%2C%22gpsjd%22%3A$longitude%2C%22gpswd%22%3A$latitude%2C%22kzl1%22%3A%221%22%2C%22kzl2%22%3A%22%22%2C%22kzl3%22%3A%22%22%2C%22kzl4%22%3A%22%22%2C%22kzl5%22%3A%22%22%2C%22kzl6%22%3A%22$province%22%2C%22kzl7%22%3A%22$city%22%2C%22kzl8%22%3A%22$district%22%2C%22kzl9%22%3A%22$street_number%22%2C%22kzl10%22%3A%22$province$district$street_number%22%2C%22kzl11%22%3A%22%22%2C%22kzl12%22%3A%22%22%2C%22kzl13%22%3A%220%22%2C%22kzl14%22%3A%22%22%2C%22kzl15%22%3A%220%22%2C%22kzl16%22%3A%22%22%2C%22kzl17%22%3A%221%22%2C%22kzl18%22%3A%220%3B%22%2C%22kzl19%22%3A%22%22%2C%22kzl20%22%3A%22%22%2C%22kzl21%22%3A%22%22%2C%22kzl22%22%3A%22%22%2C%22kzl23%22%3A%220%22%2C%22kzl24%22%3A%220%22%2C%22kzl25%22%3A%22%22%2C%22kzl26%22%3A%22%22%2C%22kzl27%22%3A%22%22%2C%22kzl28%22%3A%220%22%2C%22kzl29%22%3A%22%22%2C%22kzl30%22%3A%22%22%2C%22kzl31%22%3A%22%22%2C%22kzl32%22%3A%22$vaccine%22%2C%22kzl33%22%3A%22%22%2C%22kzl34%22%3A%7B%7D%2C%22kzl38%22%3A%22$province%22%2C%22kzl39%22%3A%22$city%22%2C%22kzl40%22%3A%22$district%22%7D%2C%22token%22%3A%22$token%22%7D" \
   --compressed)
 time=$(date "+[%Y/%m/%d-%H:%M:%S]")
 echo "$time:$res"
